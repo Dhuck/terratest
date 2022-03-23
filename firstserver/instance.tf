df262f5d499c783c8864e71569e0021badb4707f
hpolanco <hectornet80@gmail.com>
2022-03-23 13:58:36 -0600

    AWS Variables fix

--- firstserver/instance.tf
+++ firstserver/instance.tf
@@ -1,20 +1,23 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 3.0"
     }
   }
 }
 
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_KEY_ID" {}
+
 provider "aws" {
-  access_key = "xxxxxxxxxxxxxxxxxxxxxx"
-  secret_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-  region = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_KEY_ID
+  region     = "us-east-1"
 }
 
 resource "aws_instance" "hello-instance" {
-  ami = "ami-0c02fb55956c7d316"
+  ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
   tags = {
     Name = "hello-instance"
