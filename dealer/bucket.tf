3852c9c4d2583287bc4c8ab692d80fb9cd870efb
hpolanco <hectornet80@gmail.com>
2022-03-24 12:02:00 -0600

    Bucket creation exercise

--- dealer/bucket.tf
+++ dealer/bucket.tf
@@ -1,23 +1,40 @@
+#variable "AWS_ACCESS_KEY_ID" {}
+#variable "AWS_SECRET_ACCESS_KEY" {}
+
 provider "aws" {
-  region     = var.region
-  profile    = "test_profile"
-  access_key = xxxxx
-  secret_key = xxxxxx
+  region = "us-east-1"
+  #profile    = "test_profile"
+  #access_key = var.AWS_ACCESS_KEY_ID
+  #secret_key = var.AWS_SECRET_KEY_ID
+}
+
+variable "bucket" {
+  type    = string
+  default = "trfs3course"
 }
 
 /*------------bucket------------*/
 
 resource "aws_s3_bucket" "bucket1" {
   bucket = var.bucket
-  acl    = "public-read"
-  website {
-    index_document = "index.html"
+  #acl    = "public-read"
+  #website {
+  #  index_document = "index.html"
+  #}
+  tags = {
+    Name        = "sr-tag"
+    Environment = "Dev"
   }
+}
+
+variable "content" {
+  type    = string
+  default = "Text line"
 }
 
 /*------------content------------*/
 
-resource "aws_s3_bucket_object" "object1" {
+resource "aws_s3_bucket_object" "bucket1" {
   bucket       = aws_s3_bucket.bucket1.bucket
   key          = "index.html"
   acl          = "public-read"
